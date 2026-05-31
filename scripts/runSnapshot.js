5b9a57df053299c49ad513149001ed0e9ec2d38b
Kernel <m.rafiqrodzi@gmail.com>
2026-05-31 23:58:47 +0800

    feat: add dotenv configuration to runSnapshot script

--- scripts/runSnapshot.js
+++ scripts/runSnapshot.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
 const { createInventorySnapshot } = require("../services/snapshot/createInventorySnapshot");
 
 (async () => {
